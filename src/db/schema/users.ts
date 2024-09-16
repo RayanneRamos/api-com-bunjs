c696288532bb4b95e4e5af86e4130a27dd359f2e
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-09-16 11:49:55 -0300

    variaveis de ambiente no bun

--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -1,5 +1,6 @@
 import { text, timestamp, pgTable, pgEnum } from "drizzle-orm/pg-core";
 import { createId } from "@paralleldrive/cuid2";
+import { relations } from "drizzle-orm";
 
 export const userRoleEnum = pgEnum("user_role", ["manager", "customer"]);
 
@@ -13,4 +14,27 @@ export const users = pgTable("users", {
   role: userRoleEnum("role").default("customer").notNull(),
   createdAt: timestamp("created_at").notNull().defaultNow(),
   updatedAt: timestamp("updated_at").notNull().defaultNow(),
+});
+
+export const restaurants = pgTable("restaurants", {
+  id: text("id")
+    .$defaultFn(() => createId())
+    .primaryKey(),
+  name: text("name").notNull(),
+  description: text("description"),
+  managerId: text("manager_id").references(() => users.id, {
+    onDelete: "set null",
+  }),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+});
+
+export const restaurantsRelations = relations(restaurants, ({ one }) => {
+  return {
+    manager: one(users, {
+      fields: [restaurants.managerId],
+      references: [users.id],
+      relationName: "restaurant_manager",
+    }),
+  };
 });
